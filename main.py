2ab65008f85bac56a95ce28e2b6a4f59ede0af20
Grzegorz Borkowski <gb.grzegorzborkowski@gmail.com>
2019-04-11 22:12:31 +0200

    Extract lp specific functions to other file

--- main.py
+++ main.py
@@ -1,235 +1,19 @@
-# Copied from: https://github.com/lucashu1/link-prediction
-# @misc{lucas_hu_2018_1408472,
-#   author       = {Lucas Hu and
-#                   Thomas Kipf and
-#                   Gökçen Eraslan},
-#   title        = {{lucashu1/link-prediction: v0.1: FB and Twitter 
-#                    Networks}},
-#   month        = sep,
-#   year         = 2018,
-#   doi          = {10.5281/zenodo.1408472},
-#   url          = {https://doi.org/10.5281/zenodo.1408472}
-# }
-
 import networkx as nx
 import scipy.sparse as sp
 import numpy as np
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import average_precision_score
 from gensim.models import Word2Vec
 import node2vec
-from sklearn.linear_model import LogisticRegression
 from LP_arguments import LP_arguments
 from sklearn.manifold import TSNE
 import umap
 from sklearn.decomposition import PCA
 import argparse
-
-def sparse_to_tuple(sparse_mx):
-    if not sp.isspmatrix_coo(sparse_mx):
-        sparse_mx = sparse_mx.tocoo()
-    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
-    values = sparse_mx.data
-    shape = sparse_mx.shape
-    return coords, values, shape
-
-def mask_test_edges(adj, test_frac=.1, val_frac=.05, prevent_disconnect=True, verbose=False):
-    # NOTE: Splits are randomized and results might slightly deviate from reported numbers in the paper.
-
-    if verbose == True:
-        print ('preprocessing...')
-
-    # Remove diagonal elements
-    adj = adj - sp.dia_matrix((adj.diagonal()[np.newaxis, :], [0]), shape=adj.shape)
-    adj.eliminate_zeros()
-    # Check that diag is zero:
-    assert np.diag(adj.todense()).sum() == 0
-
-    g = nx.from_scipy_sparse_matrix(adj)
-    orig_num_cc = nx.number_connected_components(g)
-
-    adj_triu = sp.triu(adj) # upper triangular portion of adj matrix
-    adj_tuple = sparse_to_tuple(adj_triu) # (coords, values, shape), edges only 1 way
-    edges = adj_tuple[0] # all edges, listed only once (not 2 ways)
-    # edges_all = sparse_to_tuple(adj)[0] # ALL edges (includes both ways)
-    num_test = int(np.floor(edges.shape[0] * test_frac)) # controls how large the test set should be
-    num_val = int(np.floor(edges.shape[0] * val_frac)) # controls how alrge the validation set should be
-
-    # Store edges in list of ordered tuples (node1, node2) where node1 < node2
-    edge_tuples = [(min(edge[0], edge[1]), max(edge[0], edge[1])) for edge in edges]
-    all_edge_tuples = set(edge_tuples)
-    train_edges = set(edge_tuples) # initialize train_edges to have all edges
-    test_edges = set()
-    val_edges = set()
-
-    if verbose == True:
-        print ('generating test/val sets...')
-
-    # Iterate over shuffled edges, add to train/val sets
-    np.random.shuffle(edge_tuples)
-    for edge in edge_tuples:
-        # print edge
-        node1 = edge[0]
-        node2 = edge[1]
-
-        # If removing edge would disconnect a connected component, backtrack and move on
-        g.remove_edge(node1, node2)
-        if prevent_disconnect == True:
-            if nx.number_connected_components(g) > orig_num_cc:
-                g.add_edge(node1, node2)
-                continue
-
-        # Fill test_edges first
-        if len(test_edges) < num_test:
-            test_edges.add(edge)
-            train_edges.remove(edge)
-
-        # Then, fill val_edges
-        elif len(val_edges) < num_val:
-            val_edges.add(edge)
-            train_edges.remove(edge)
-
-        # Both edge lists full --> break loop
-        elif len(test_edges) == num_test and len(val_edges) == num_val:
-            break
-
-    if (len(val_edges) < num_val or len(test_edges) < num_test):
-        print ("WARNING: not enough removable edges to perform full train-test split!")
-        print ("Num. (test, val) edges requested: (", num_test, ", ", num_val, ")")
-        print ("Num. (test, val) edges returned: (", len(test_edges), ", ", len(val_edges), ")")
-
-    if prevent_disconnect == True:
-        assert nx.number_connected_components(g) == orig_num_cc
-
-    if verbose == True:
-        print ('creating false test edges...')
-
-    test_edges_false = set()
-    while len(test_edges_false) < num_test:
-        idx_i = np.random.randint(0, adj.shape[0])
-        idx_j = np.random.randint(0, adj.shape[0])
-        if idx_i == idx_j:
-            continue
-
-        false_edge = (min(idx_i, idx_j), max(idx_i, idx_j))
-
-        # Make sure false_edge not an actual edge, and not a repeat
-        if false_edge in all_edge_tuples:
-            continue
-        if false_edge in test_edges_false:
-            continue
-
-        test_edges_false.add(false_edge)
-
-    if verbose == True:
-        print ('creating false val edges...')
-
-    val_edges_false = set()
-    while len(val_edges_false) < num_val:
-        idx_i = np.random.randint(0, adj.shape[0])
-        idx_j = np.random.randint(0, adj.shape[0])
-        if idx_i == idx_j:
-            continue
-
-        false_edge = (min(idx_i, idx_j), max(idx_i, idx_j))
-
-        # Make sure false_edge in not an actual edge, not in test_edges_false, not a repeat
-        if false_edge in all_edge_tuples or \
-            false_edge in test_edges_false or \
-            false_edge in val_edges_false:
-            continue
-            
-        val_edges_false.add(false_edge)
-
-    if verbose == True:
-        print ('creating false train edges...')
-
-    train_edges_false = set()
-    while len(train_edges_false) < len(train_edges):
-        idx_i = np.random.randint(0, adj.shape[0])
-        idx_j = np.random.randint(0, adj.shape[0])
-        if idx_i == idx_j:
-            continue
-
-        false_edge = (min(idx_i, idx_j), max(idx_i, idx_j))
-
-        # Make sure false_edge in not an actual edge, not in test_edges_false, 
-            # not in val_edges_false, not a repeat
-        if false_edge in all_edge_tuples or \
-            false_edge in test_edges_false or \
-            false_edge in val_edges_false or \
-            false_edge in train_edges_false:
-            continue
-
-        train_edges_false.add(false_edge)
-
-    if verbose == True:
-        print ('final checks for disjointness...')
-
-    # assert: false_edges are actually false (not in all_edge_tuples)
-    assert test_edges_false.isdisjoint(all_edge_tuples)
-    assert val_edges_false.isdisjoint(all_edge_tuples)
-    assert train_edges_false.isdisjoint(all_edge_tuples)
-
-    # assert: test, val, train false edges disjoint
-    assert test_edges_false.isdisjoint(val_edges_false)
-    assert test_edges_false.isdisjoint(train_edges_false)
-    assert val_edges_false.isdisjoint(train_edges_false)
-
-    # assert: test, val, train positive edges disjoint
-    assert val_edges.isdisjoint(train_edges)
-    assert test_edges.isdisjoint(train_edges)
-    assert val_edges.isdisjoint(test_edges)
-
-    if verbose == True:
-        print ('creating adj_train...')
-
-    # Re-build adj matrix using remaining graph
-    adj_train = nx.adjacency_matrix(g)
-
-    # Convert edge-lists to numpy arrays
-    train_edges = np.array([list(edge_tuple) for edge_tuple in train_edges])
-    train_edges_false = np.array([list(edge_tuple) for edge_tuple in train_edges_false])
-    val_edges = np.array([list(edge_tuple) for edge_tuple in val_edges])
-    val_edges_false = np.array([list(edge_tuple) for edge_tuple in val_edges_false])
-    test_edges = np.array([list(edge_tuple) for edge_tuple in test_edges])
-    test_edges_false = np.array([list(edge_tuple) for edge_tuple in test_edges_false])
-
-    if verbose == True:
-        print ('Done with train-test split!')
-        print ('')
-
-    # NOTE: these edge lists only contain single direction of edge!
-    return adj_train, train_edges, train_edges_false, \
-        val_edges, val_edges_false, test_edges, test_edges_false
-
-def get_roc_score(adj_sparse, edges_pos, edges_neg, score_matrix):
-    # Store positive edge predictions, actual values
-    preds_pos = []
-    pos = []
-    for edge in edges_pos:
-        preds_pos.append(score_matrix[edge[0], edge[1]]) # predicted score
-        pos.append(adj_sparse[edge[0], edge[1]]) # actual value (1 for positive)
-        
-    # Store negative edge predictions, actual values
-    preds_neg = []
-    neg = []
-    for edge in edges_neg:
-        preds_neg.append(score_matrix[edge[0], edge[1]]) # predicted score
-        neg.append(adj_sparse[edge[0], edge[1]]) # actual value (0 for negative)
-        
-    # Calculate scores
-    preds_all = np.hstack([preds_pos, preds_neg])
-    labels_all = np.hstack([np.ones(len(preds_pos)), np.zeros(len(preds_neg))])
-    roc_score = roc_auc_score(labels_all, preds_all)
-    ap_score = average_precision_score(labels_all, preds_all)
-    return roc_score, ap_score
-
+from link_prediction_helpers import *
 from collections import namedtuple
 
 MethodResult = namedtuple('MethodResult', ['methodName', 'testROC', 'testPC'])
 
-class Results():
+class LatexResults():
 
     def __init__(self, number_of_nodes, training_edges, test_edges, list_of_methods_result):
         self.number_of_nodes = number_of_nodes
@@ -307,7 +91,6 @@ def calculate(min_degree, file_path="graph.graph"):
     # Calculate ROC AUC and Average Precision
     pa_roc, pa_ap = get_roc_score(adj_sparse, test_edges, test_edges_false, pa_matrix)
 
-    
     P = 1 # Return hyperparameter
     Q = 1 # In-out hyperparameter
     WINDOW_SIZE = 12 # Context size for optimization
@@ -372,76 +155,10 @@ def calculate(min_degree, file_path="graph.graph"):
 
         methods_list.append(MethodResult(key, test_roc, test_ap))
     
-    result = Results(adj_sparse.shape[0], len(train_edges), len(test_edges), methods_list)
+    result = LatexResults(adj_sparse.shape[0], len(train_edges), len(test_edges), methods_list)
     
     with open("results.txt", "a") as file:
         file.write(result.get_latex_representation())
-
-
-def link_prediction_on_embedding(lp_arg):
-    emb_mappings = lp_arg.emb_mappings
-    adj_sparse = lp_arg.adj_sparse
-    train_edges = lp_arg.train_edges
-    train_edges_false = lp_arg.train_edges_false
-    val_edges = lp_arg.val_edges
-    val_edges_false = lp_arg.val_edges_false
-    test_edges = lp_arg.test_edges
-    test_edges_false = lp_arg.test_edges_false
-    emb_matrix = lp_arg.matrix
-
-    # Train-set edge embeddings
-    pos_train_edge_embs = get_edge_embeddings(train_edges,emb_matrix)
-    neg_train_edge_embs = get_edge_embeddings(train_edges_false,emb_matrix)
-    train_edge_embs = np.concatenate([pos_train_edge_embs, neg_train_edge_embs])
-
-    # Create train-set edge labels: 1 = real edge, 0 = false edge
-    train_edge_labels = np.concatenate([np.ones(len(train_edges)), np.zeros(len(train_edges_false))])
-
-    # Val-set edge embeddings, labels
-    pos_val_edge_embs = get_edge_embeddings(val_edges,emb_matrix)
-    neg_val_edge_embs = get_edge_embeddings(val_edges_false,emb_matrix)
-    val_edge_embs = np.concatenate([pos_val_edge_embs, neg_val_edge_embs])
-    val_edge_labels = np.concatenate([np.ones(len(val_edges)), np.zeros(len(val_edges_false))])
-
-    # Test-set edge embeddings, labels
-    pos_test_edge_embs = get_edge_embeddings(test_edges,emb_matrix)
-    neg_test_edge_embs = get_edge_embeddings(test_edges_false,emb_matrix)
-    test_edge_embs = np.concatenate([pos_test_edge_embs, neg_test_edge_embs])
-
-    # Create val-set edge labels: 1 = real edge, 0 = false edge
-    test_edge_labels = np.concatenate([np.ones(len(test_edges)), np.zeros(len(test_edges_false))])
-
-    # Train logistic regression classifier on train-set edge embeddings
-    edge_classifier = LogisticRegression(random_state=0)
-    edge_classifier.fit(train_edge_embs, train_edge_labels)
-
-    # Predicted edge scores: probability of being of class "1" (real edge)
-    val_preds = edge_classifier.predict_proba(val_edge_embs)[:, 1]
-    val_roc = roc_auc_score(val_edge_labels, val_preds)
-    val_ap = average_precision_score(val_edge_labels, val_preds)
-
-    # Predicted edge scores: probability of being of class "1" (real edge)
-    test_preds = edge_classifier.predict_proba(test_edge_embs)[:, 1]
-    test_roc = roc_auc_score(test_edge_labels, test_preds)
-    test_ap = average_precision_score(test_edge_labels, test_preds)
-
-    return val_roc, val_ap, test_roc, test_ap
-
-
-    # Generate bootstrapped edge embeddings (as is done in node2vec paper)
-    # Edge embedding for (v1, v2) = hadamard product of node embeddings for v1, v2
-def get_edge_embeddings(edge_list,emb_matrix):
-    embs = []
-    for edge in edge_list:
-        node1 = edge[0]
-        node2 = edge[1]
-        emb1 = emb_matrix[node1]
-        emb2 = emb_matrix[node2]
-        edge_emb = np.multiply(emb1, emb2)
-        embs.append(edge_emb)
-    embs = np.array(embs)
-    return embs
-    
 
 if __name__ == "__main__":
 
